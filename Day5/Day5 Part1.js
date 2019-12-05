f4420b2d13e62a56ba4937340ffe904a63a0462e
Thomas Richards <Deskbot@users.noreply.github.com>
2019-12-05 21:38:07 +0000

    Add Day5 Part1

--- Day5/Day5 Part1.js
+++ Day5/Day5 Part1.js
@@ -2,63 +2,58 @@ const fs = require("fs");
 
 const file = fs.readFileSync("input.txt").toString();
 
-const targetOutput = 19690720;
-
-const baseProgram = file.split(",")
+const program = file.split(",")
+    .filter(line => line !== "")
     .map(str => parseInt(str));
 
-for (let noun = 0; noun < 100; noun++) {
-    for (let verb = 0; verb < 100; verb++) {
-        const program = [...baseProgram];
-        program[1] = noun;
-        program[2] = verb;
+const input = 1;
+const output = runProgram(input, program);
 
-        const result = runProgram(program)[0];
+console.log(output);
 
-        if (result === targetOutput) {
-            console.log(`Noun ${noun}, Verb ${verb}, Together ${noun * 100 + verb}`);
-            break;
-        }
-    }
-}
-
-function runProgram(program) {
+function runProgram(input, program) {
     let address = 0;
+    let output;
 
     while (true) {
-        const code = program[address];
+        const command = program[address];
+        const opCode = command % 100;
+        const positionMode1 = command % 1000 - opCode;
+        const positionMode2 = command % 10000 - positionMode1 - opCode;
+        // const positionMode3 = command % 100000 - positionMode2 - positionMode1 - opCode;
 
-        if (code == 99) {
+        if (opCode == 99) {
             break;
         }
 
-        const operand1Address = program[address + 1];
-        const operand2Address = program[address + 2];
-        const operand1 = program[operand1Address];
-        const operand2 = program[operand2Address];
-        const targetAddress = program[address + 3];
-
-        if (code == 1) {
-            program[targetAddress] = operand1 + operand2;
-            address += 4;
-        } else if (code == 2) {
-            program[targetAddress] = operand1 * operand2;
+        if (opCode === 1 || opCode === 2) {
+            const param1 = program[address + 1];
+            const param2 = program[address + 2];
+            const operand1 = positionMode1 === 0 ? program[param1] : param1;
+            const operand2 = positionMode2 === 0 ? program[param2] : param2;
+            const targetAddress = program[address + 3];
             address += 4;
 
-        } else if (code == 3) {
-            program[targetAddress] = operand1 * operand2;
-            address += 4;
+            if (opCode === 1) {
+                program[targetAddress] = operand1 + operand2;
+            } else {
+                program[targetAddress] = operand1 * operand2;
+            }
 
-        } else if (code == 4) {
-            program[targetAddress] = operand1 * operand2;
-            address += 4;
+        } else if (opCode === 3) {
+            const operandAddress = program[address + 1];
+            program[operandAddress] = input;
+            address += 2;
+
+        } else if (opCode === 4) {
+            const operandAddress = program[address + 1];
+            output = program[operandAddress];
+            address += 2;
 
         } else {
-            throw `Unrecognised opcode ${code} at address ${address} in program ${program}.`;
+            throw `Unrecognised opcode ${opCode} at address ${address} in program ${program}.`;
         }
-
-
     }
 
-    return program;
+    return output;
 }
