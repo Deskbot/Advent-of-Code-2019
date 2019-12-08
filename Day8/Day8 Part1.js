f78b5b9d1a5b3f44a5f1dbd3cd05e7af573590c9
Thomas Richards <Deskbot@users.noreply.github.com>
2019-12-08 10:14:28 +0000

    Add Day8 Part1

---
+++ Day8/Day8 Part1.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+const file = fs.readFileSync("input.txt").toString();
+
+const image = file.split("")
+    .filter(str => str.trim() !== "")
+    .map(str => parseInt(str));
+
+const width = 25;
+const height = 6;
+const pixelsInLayer = width * height;
+
+const layers = [];
+while (image.length > 0) {
+    layers.push(image.splice(0, pixelsInLayer));
+}
+
+layers.sort((layer1, layer2) => {
+    return countEqual(layer1, 0) - countEqual(layer2, 0);
+});
+
+const layerWithFewestZeros = layers[0];
+
+console.log(countEqual(layerWithFewestZeros, 1) * countEqual(layerWithFewestZeros, 2));
+
+function countEqual(arr, valToEqual) {
+    return arr.filter(val => val === valToEqual).length;
+}
