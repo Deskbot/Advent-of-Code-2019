1c3d8c04cf9f2fa281e8419787e4764428960fd7
Thomas Richards <Deskbot@users.noreply.github.com>
2019-12-08 11:08:44 +0000

    Add Day8 Part2

--- Day8/Day8.js
+++ Day8/Day8.js
@@ -1,3 +1,4 @@
+const bmpjs = require("bmp-js");
 const fs = require("fs");
 
 const file = fs.readFileSync("input.txt").toString();
@@ -16,6 +17,7 @@ while (image.length > 0) {
 }
 
 part1(layers.slice());
+part2(layers.slice());
 
 function countEqual(arr, valToEqual) {
     return arr.filter(val => val === valToEqual).length;
@@ -29,4 +31,38 @@ function part1(layers) {
     const layerWithFewestZeros = layers[0];
 
     console.log(countEqual(layerWithFewestZeros, 1) * countEqual(layerWithFewestZeros, 2));
+}
+
+function part2(layers) {
+    let finalImage = [];
+    for (const layer of layers) {
+        for (let i = 0; i < pixelsInLayer; i++) {
+            if (finalImage[i] === undefined && layer[i] !== 2) {
+                finalImage[i] = layer[i];
+            }
+        }
+    }
+
+    finalImage = finalImage.map((pixel) => {
+        switch (pixel) {
+            case 0: return 0x00000000;
+            case 1: return 0x00FFFFFF;
+        }
+        return 0xFF000000;
+    });
+
+    const data = Buffer.alloc(layers.length * pixelsInLayer * 32);
+
+    let offset = 0;
+    for (const pixel of finalImage) {
+        offset = data.writeUInt32BE(pixel, offset);
+    }
+
+    const bmp = bmpjs.encode({
+        data,
+        width,
+        height,
+    });
+
+    fs.writeFileSync("output.bmp", bmp.data);
 }
