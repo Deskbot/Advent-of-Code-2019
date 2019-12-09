e8b9a934ccec17613cda9631c509a916a556f23d
Thomas Richards <Deskbot@users.noreply.github.com>
2019-12-09 20:29:02 +0000

    Fix some of the brokeness

--- IntCode.js
+++ IntCode.js
@@ -1,16 +1,19 @@
 class Program {
     constructor(initialState) {
         this.address = 0;
-        this.state = initialState;
+        this.state = new Map();
+        initialState.forEach((state, i) => {
+            this.state.set(i, state);
+        });
         this.inputs = [];
         this.halted = false;
         this.relativeBase = 0;
     }
 
     getValue(addressMode, param) {
-        if (addressMode === 0) return this.state[param];
+        if (addressMode === 0) return this.read(param);
         if (addressMode === 1) return param;
-        if (addressMode === 2) return this.state[param + this.relativeBase];
+        if (addressMode === 2) return this.read(param + this.relativeBase);
         throw `Unexpected addressMode ${addressMode} in program ${JSON.stringify(this)}`;
     }
 
@@ -19,7 +22,7 @@ class Program {
     }
 
     read(address) {
-        if (this.state[address]) return this.state[address];
+        if (this.state.get(address) !== undefined) return this.state.get(address);
 
         return 0;
     }
@@ -30,9 +33,9 @@ class Program {
         while (true) {
             const command = this.read(this.address);
             const opCode = command % 100;
-            const positionMode1 = command % 1000 - opCode;
-            const positionMode2 = command % 10000 - positionMode1 - opCode;
-            const positionMode3 = command % 100000 - positionMode2 - positionMode1 - opCode;
+            const positionMode1 = Math.floor((command % 1000) /   100);
+            const positionMode2 = Math.floor((command % 10000) /  1000);
+            const positionMode3 = Math.floor((command % 100000) / 10000);
 
             if (opCode == 99) {
                 this.halted = true;
@@ -43,25 +46,28 @@ class Program {
                 const param1 = this.read(this.address + 1);
                 const param2 = this.read(this.address + 2);
                 const param3 = this.read(this.address + 3);
-                const operand1 = positionMode1 === 0 ? this.read(param1) : param1;
-                const operand2 = positionMode2 === 0 ? this.read(param2) : param2;
-                // const targetAddress = positionMode3 === 0 ? this.read(param3): param3;
-                const targetAddress = this.read(this.address + 3);
+                const operand1 = this.getValue(positionMode1, param1);
+                const operand2 = this.getValue(positionMode2, param2);
+                const targetAddress = this.getValue(positionMode3, param3);
                 this.address += 4;
 
+                let toSet;
+
                 if (opCode === 1) {
-                    this.state[targetAddress] = operand1 + operand2;
+                    toSet = operand1 + operand2;
                 } else if (opCode === 2) {
-                    this.state[targetAddress] = operand1 * operand2;
+                    toSet = operand1 * operand2;
                 } else if (opCode === 7) {
-                    this.state[targetAddress] = operand1 < operand2 ? 1 : 0;
+                    toSet = operand1 < operand2 ? 1 : 0;
                 } else if (opCode === 8) {
-                    this.state[targetAddress] = operand1 === operand2 ? 1 : 0;
+                    toSet = operand1 === operand2 ? 1 : 0;
                 }
+
+                this.state.set(targetAddress, toSet);
 
             } else if (opCode === 3) {
                 const operandAddress = this.read(this.address + 1);
-                this.state[operandAddress] = this.inputs.shift();
+                this.state.set(operandAddress, this.inputs.shift());
                 this.address += 2;
 
             } else if (opCode === 4) {
@@ -72,8 +78,8 @@ class Program {
             } else if (opCode === 5 || opCode === 6) {
                 const param1 = this.read(this.address + 1);
                 const param2 = this.read(this.address + 2);
-                const testParam = positionMode1 === 0 ? this.read(param1) : param1;
-                const goToParam = positionMode2 === 0 ? this.read(param2) : param2;
+                const testParam = this.getValue(positionMode1, param1);
+                const goToParam = this.getValue(positionMode2, param2);
                 this.address += 3;
 
                 if (opCode === 5) {
@@ -89,10 +95,10 @@ class Program {
             } else if (opCode === 9) {
                 const param = this.read(this.address + 1);
                 this.relativeBase += this.getValue(positionMode1, param);
-                this.address += 1;
+                this.address += 2;
 
             } else {
-                throw `Unrecognised opcode ${opCode} at address ${this.address} in program ${this.state}.`;
+                throw `Unrecognised opcode ${opCode} at address ${this.address} in program ${[...this.state]}.`;
             }
         }
     }
