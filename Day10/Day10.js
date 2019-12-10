b3fd3f1fe5b846cf2b8de0bd1f2c802aae3d30ef
Thomas Richards <Deskbot@users.noreply.github.com>
2019-12-10 13:00:49 +0000

    Add line angle method

--- Day10/Day10.js
+++ Day10/Day10.js
@@ -1,17 +1,38 @@
 const fs = require("fs");
 
-const file = fs.readFileSync("input.txt").toString();
-const grid = file.split("\n")
-    .filter(line => line !== "")
-    .map(line => line.split(""));
+class Line {
+    constructor([x1,y1], [x2,y2]) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
 
-const asteroids = [];
-for (const [x, row] of grid.entries()) {
-    for (const [y, content] of row.entries()) {
-        if (content === "#") {
-            asteroids.push([x,y]);
-        }
+    get angle() {
+        return Math.atan((x1 - x2) / (y1 - y2)); // angle from x direction anti-clockwise
     }
 }
 
-console.log(asteroids)
+main();
+
+function main() {
+    const file = fs.readFileSync("input.txt").toString();
+    const grid = file.split("\n")
+        .filter(line => line !== "")
+        .map(line => line.split(""));
+
+    const asteroids = [];
+    for (const [x, row] of grid.entries()) {
+        for (const [y, content] of row.entries()) {
+            if (content === "#") {
+                asteroids.push([x, y]);
+            }
+        }
+    }
+
+    for (const asteroid of asteroids) {
+
+    }
+
+    console.log(asteroids)
+}
