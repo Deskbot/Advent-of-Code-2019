5b8683d91ad899768add29fb752719071f0b47ce
Thomas Richards <Deskbot@users.noreply.github.com>
2019-12-12 23:54:18 +0000

    Add energy and phix some fiz

--- Day12/Day12.js
+++ Day12/Day12.js
@@ -6,12 +6,14 @@
 
 const axes = ["x", "y", "z"];
 
-const moons = [
-    { name: "Io",       pos: { x:   0, y:  6, z:  1 }, vel: { x: 0, y: 0, x: 0 } },
-    { name: "Europa",   pos: { x:   4, y:  4, z: 19 }, vel: { x: 0, y: 0, x: 0 } },
-    { name: "Ganymede", pos: { x: -11, y:  1, z:  8 }, vel: { x: 0, y: 0, x: 0 } },
-    { name: "Callisto", pos: { x:   2, y: 19, z: 15 }, vel: { x: 0, y: 0, x: 0 } }
+const moonStartStates = [
+    { name: "Io",       pos: { x:   0, y:  6, z:  1 }, vel: { x: 0, y: 0, z: 0 } },
+    { name: "Europa",   pos: { x:   4, y:  4, z: 19 }, vel: { x: 0, y: 0, z: 0 } },
+    { name: "Ganymede", pos: { x: -11, y:  1, z:  8 }, vel: { x: 0, y: 0, z: 0 } },
+    { name: "Callisto", pos: { x:   2, y: 19, z: 15 }, vel: { x: 0, y: 0, z: 0 } }
 ];
+
+part1();
 
 function allPairs(list) {
     return list.flatMap((item, i) => {
@@ -20,15 +22,15 @@ function allPairs(list) {
 }
 
 function applyGravity(moons) {
-    const newMoons = JSON.parse(JSON.stringify(moons));
+    const newMoons = deepCopy(moons);
     for (const [m1, m2] of allPairs(moons)) {
         for (const axis of axes) {
-            if (m1[axis] < m2[axis]) {
-                newMoons[m1.name].vel[axis]++;
-                newMoons[m2.name].vel[axis]--;
+            if (m1.pos[axis] < m2.pos[axis]) {
+                newMoons[m1.name].pos[axis]++;
+                newMoons[m2.name].pos[axis]--;
             } else if (m1[axis] > m2[axis]) {
-                newMoons[m1.name].vel[axis]--;
-                newMoons[m2.name].vel[axis]++;
+                newMoons[m1.name].pos[axis]--;
+                newMoons[m2.name].pos[axis]++;
             }
         }
     }
@@ -36,7 +38,7 @@ function applyGravity(moons) {
 }
 
 function applyVelocity(moons) {
-    const newMoons = JSON.parse(JSON.stringify(moons));
+    const newMoons = deepCopy(moons);
     for (const moon of newMoons) {
         for (axis of axes) {
             moon.pos[axis] += moon.vel[axis];
@@ -44,3 +46,36 @@ function applyVelocity(moons) {
     }
     return newMoons;
 }
+
+function deepCopy(basicObject) {
+    return JSON.parse(JSON.stringify(basicObject));
+}
+
+function energy(moon) {
+    return kineticEnery(moon) * potentialEnergy(moon);
+}
+
+function kineticEnery(moon) {
+    return Math.abs(moon.vel.x)
+        + Math.abs(moon.vel.y)
+        + Math.abs(moon.vel.z);
+}
+
+function potentialEnergy(moon) {
+    return Math.abs(moon.pos.x)
+        + Math.abs(moon.pos.y)
+        + Math.abs(moon.pos.z);
+}
+
+function part1() {
+    let moons = deepCopy(moonStartStates);
+    for (let i = 1; i <= 1000; i++) {
+        moons = applyGravity(moons);
+        moons = applyVelocity(moons);
+    }
+
+    const totalMoonEnergy = moons.reduce((tot, moon) => tot + energy(moon), 0);
+
+    console.log(totalMoonEnergy);
+}
+
