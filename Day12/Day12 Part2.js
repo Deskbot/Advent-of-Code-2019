e1a83db2266939d47f3cd90c1892e943ffa83aaa
Thomas Richards <Deskbot@users.noreply.github.com>
2019-12-14 20:52:17 +0000

    More

--- Day12/Day12 Part2.js
+++ Day12/Day12 Part2.js
@@ -13,40 +13,74 @@ const moonStartStates = {
     "Callisto": { pos: { x: 2, y: 19, z: 15 }, vel: { x: 0, y: 0, z: 0 } }
 };
 
+const axis = "x"
+
 part2();
+
+function allPairs(list) {
+    return list.flatMap((item, i) => {
+        return list.slice(i + 1).map(otherItem => [item, otherItem]);
+    });
+}
 
 function applyGravity(moons) {
     const newMoons = deepCopy(moons);
-
-    for (const axis of axes) {
-        const moonsAlongAxis = newMoons.sort((moon1, moon2) => moon1.vel[axis] - moon2.vel[axis]);
-
-        // adjust velocities by their rank
-        for (const [moon, i] of moonsAlongAxis.entries()) {
-            moon.vel[axis] = i * (-1) + moonsAlongAxis.length;
+    for (const [m1, m2] of allPairs(Object.keys(newMoons))) {
+        if (moons[m1].pos[axis] < moons[m2].pos[axis]) {
+            newMoons[m1].vel[axis]++;
+            newMoons[m2].vel[axis]--;
+        } else if (moons[m1].pos[axis] > moons[m2].pos[axis]) {
+            newMoons[m1].vel[axis]--;
+            newMoons[m2].vel[axis]++;
         }
+        // for (const axis of axes) {
+        // }
     }
-
     return newMoons;
 }
 
 function applyVelocity(moons) {
     const newMoons = deepCopy(moons);
     for (const moonName in newMoons) {
-        for (axis of axes) {
-            newMoons[moonName].pos[axis] += moons[moonName].vel[axis];
-        }
+        newMoons[moonName].pos[axis] += moons[moonName].vel[axis];
+        // for (axis of axes) {
+        // }
     }
     return newMoons;
 }
 
-function deepCopy(basicObject) {
-    return JSON.parse(JSON.stringify(basicObject));
+// taken from 9th example in https://jsperf.com/deep-cloning-of-objects/51
+function deepCopy(obj) {
+    var i, len, ret;
+
+    if (typeof obj !== 'object' || obj === null) {
+        return obj;
+    }
+
+    if (Array.isArray(obj)) {
+        ret = [];
+        len = obj.length;
+        for (i = 0; i < len; i++) {
+            ret.push((typeof obj[i] === 'object' && obj[i] !== null) ? deepCopy(obj[i]) : obj[i]);
+        }
+    } else {
+        ret = {};
+        for (i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                ret[i] = (typeof obj[i] === 'object' && obj[i] !== null) ? deepCopy(obj[i]) : obj[i];
+            }
+        }
+    }
+
+    return ret;
 }
+// function deepCopy(basicObject) {
+//     return JSON.parse(JSON.stringify(basicObject));
+// }
 
 function moonStatesAreEqual(moons1, moons2) {
     for (const moonName in moons1) {
-        if (moonsEqual(moons1[moonName], moons2[moonName])) {
+        if (!moonsEqual(moons1[moonName], moons2[moonName])) {
             return false;
         }
     }
@@ -54,23 +88,33 @@ function moonStatesAreEqual(moons1, moons2) {
 }
 
 function moonsEqual(m1, m2) {
-    for (const axis of axes) {
-        if (m1[axis] !== m2[axis]) return false;
+    if (m1.pos[axis] !== m2.pos[axis]
+        || m1.vel[axis] !== m2.vel[axis]
+    ) {
+        return false;
     }
+    // for (const axis of axes) {
+    // }
 
     return true;
 }
 
 function part2() {
+    const allMoonStates = [moonStartStates];
     let moons = deepCopy(moonStartStates);
 
     for (var i = 1; i < Infinity; i++) {
+        console.log(i)
         moons = applyGravity(moons);
         moons = applyVelocity(moons);
 
-        if (moonStatesAreEqual(moons, moonStartStates)) {
-            break;
+        for (const prevState of allMoonStates) {
+            if (moonStatesAreEqual(moons, prevState)) {
+                break;
+            }
         }
+
+        allMoonStates.push(moons);
     }
 
     console.log(i);
