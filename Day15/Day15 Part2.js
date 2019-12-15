5ba5b1b59a0760da729953eb946262fbd9e40df7
Thomas Richards <Deskbot@users.noreply.github.com>
2019-12-15 13:59:46 +0000

    Wrap maze finding in func

--- Day15/Day15 Part2.js
+++ Day15/Day15 Part2.js
@@ -1,96 +1,144 @@
 const fs = require("fs");
-const readline = require('readline');
 const { Program } = require("../IntCode");
 const { World } = require("./World");
 
-const oppositeDir = {
-    1: 2,
-    2: 1,
-    3: 4,
-    4: 3,
-}
+// generated from running the droid
+const maze = `
+#########################################
+#...#.........#.....#.......#...#.......#
+#.#.#.#.#####.###.#.#######.#.#.#...#...#
+#.#...#...#.......#.........#.#.#...#...#
+#.#...###.###############.###.#.###.#.#.#
+#.#...#.#...#...#.#.......#...#.....#.#.#
+#.#.#.#.###.#.#.#.#.#######.....#...#.#.#
+#.#.#.#...#...#.#...#.......#.###...#.#.#
+#...#...#.#.....#.###.#...#...#...#...#.#
+#...#...#.#.....#.#...#.#.#.###...#...#.#
+#########.#.#######.#...#.#...#.#######.#
+#...#...#.#.......#.#.###.#.###.#.......#
+#.#.###.#.#####...#.#.#...#.#...#.#####.#
+#.#...#.#.#...#...#.#.#.###.#...#.#...#.#
+#.###.#.#.###.#.###.#.#.#...#.###.#.#.###
+#.#.....#.....#.....#.#.#.###.#...#.#...#
+#.###.#######.#####...#.#.#...#.###.#...#
+#...#.#...#...#...#.###.#.#...#.#...#...#
+###.###.#.#.#####.#.#...#.#.#####...#.###
+#...#...#...#...#.#.#D..#.#.#.....###...#
+#.###.#######.#.#.#...#.#.#.#...#.......#
+#.....#.......#...#####.###...###.......#
+#.#########.#####.#####.#.........#######
+#.#.........#.......#...#.........#.....#
+#.#.###.#####.#######.#...#############.#
+#.#.#...#...#.....#...###.#...#.........#
+#.#.#.###.#.#####.#.#.....#.###.###.###.#
+#.#.#.#...#.#.....#.#.....#.....#...#...#
+#.###.#.###.#.#####...###########.###.###
+#.....#.#.#.#...#.#...#...........#...#.#
+#######.#.#.###.#.#####.###.#######.###.#
+#.#.....#...#...#...#.....#...#...#...#.#
+#.#.#.###.###.###.#.#########.#.#####.#.#
+#...#...#.#...#...#...#.....#.#.....#.#.#
+#.#####.###.#####.###.#.###.#.#.###.#.#.#
+#.#...#.....#...#...#.#.#...#.#...#.#...#
+#.###.#######.#.###.#.#.###.#.#.###.###.#
+#...#.........#...#.#.#...#...#.#.....#.#
+###.#.###########.#.#.###.#####.#.#####.#
+#.....#.............#.....#.....#.......#
+ ##### ############# ##### ##### #######
+`
 
-const leftOf = {
-    1: 3,
-    2: 4,
-    3: 2,
-    4: 1,
-}
+// outputMaze();
 
-const rightOf = {
-    1: 4,
-    2: 3,
-    3: 1,
-    4: 2,
-}
-
-const code = fs.readFileSync("input.txt").toString()
-    .split(",")
-    .filter(num => num !== "")
-    .map(num => parseInt(num));
-
-const game = new Program(code);
-const runner = game.run();
-
-const world = new World();
-const moves = [];
-let moveDir = 1;
-let count = 0;
-
-while (true) {
-    count++;
-    console.log(world.grid.toString());
-    console.log(moves.length)
-
-    game.input(moveDir);
-
-    const status = step(runner, moveDir);
-
-    if (status === 2 || status === true) break;
-
-    if (status === 0) {
-        moveDir = rightOf[moveDir];
-    } else {
-        moveDir = leftOf[moveDir];
+function outputMaze() {
+    const oppositeDir = {
+        1: 2,
+        2: 1,
+        3: 4,
+        4: 3,
     }
-}
 
-console.log(world.grid.toString());
-console.log(moves);
-console.log(moves.length + 1) // dunno why I need to add 1, tried it and it was right
+    const leftOf = {
+        1: 3,
+        2: 4,
+        3: 2,
+        4: 1,
+    }
 
-function step(runner, dir) {
-    const { value: status, done } = runner.next();
+    const rightOf = {
+        1: 4,
+        2: 3,
+        3: 1,
+        4: 2,
+    }
 
-    if (done) return true;
+    const code = fs.readFileSync("input.txt").toString()
+        .split(",")
+        .filter(num => num !== "")
+        .map(num => parseInt(num));
 
-    if (status === 0) {
-        if (dir === 1) {
-            world.wallNorth();
-        } else if (dir === 2) {
-            world.wallSouth();
-        } else if (dir === 3) {
-            world.wallWest();
-        } else { // 4
-            world.wallEast();
-        }
-    } else if (status === 1) {
-        if (dir === 1) {
-            world.moveNorth();
-        } else if (dir === 2) {
-            world.moveSouth();
-        } else if (dir === 3) {
-            world.moveWest();
-        } else { // 4
-            world.moveEast();
-        }
+    const game = new Program(code);
+    const runner = game.run();
 
-        if (dir === oppositeDir[moves[moves.length - 1]]) {
-            moves.pop();
+    const world = new World();
+    const moves = [];
+    let moveDir = 1;
+    let firstMove = true;
+
+    while (true) {
+        console.log(world.grid.toString());
+        console.log(moves.length)
+
+        game.input(moveDir);
+
+        const status = step(runner, moveDir);
+
+        if (status === true || !firstMove && world.x === 0 && world.y === 0) break;
+
+        if (status === 0) {
+            moveDir = rightOf[moveDir];
         } else {
-            moves.push(dir);
+            firstMove = false;
+            moveDir = leftOf[moveDir];
         }
     }
 
-    return status;
+    console.log(world.grid.toString());
+    console.log(moves);
+    console.log(moves.length + 1) // dunno why I need to add 1, tried it and it was right
+
+    function step(runner, dir) {
+        const { value: status, done } = runner.next();
+
+        if (done) return true;
+
+        if (status === 0) {
+            if (dir === 1) {
+                world.wallNorth();
+            } else if (dir === 2) {
+                world.wallSouth();
+            } else if (dir === 3) {
+                world.wallWest();
+            } else { // 4
+                world.wallEast();
+            }
+        } else if (status === 1) {
+            if (dir === 1) {
+                world.moveNorth();
+            } else if (dir === 2) {
+                world.moveSouth();
+            } else if (dir === 3) {
+                world.moveWest();
+            } else { // 4
+                world.moveEast();
+            }
+
+            if (dir === oppositeDir[moves[moves.length - 1]]) {
+                moves.pop();
+            } else {
+                moves.push(dir);
+            }
+        }
+
+        return status;
+    }
 }
